d491cfd56db7047052391a2832854c3d31303c56
ImpossiblePasta <joshuamedves@gmail.com>
2024-03-09 14:42:09 -0500

    Improved card loading and all links added

--- src/components/card_card.js
+++ src/components/card_card.js
@@ -3,32 +3,36 @@ import { Text, View } from 'react-native';
 import './card_card.css';
 import tabImg from '../assets/icons/newtab.png';
 
-function CardCard({title, link, image, button_name}){
+function CardCard({title, link, image}){
 
   const [picture, setPicture] = useState(image);
 
-  async function reloadImg() {
+  async function reloadImg(link) {
     return fetch(link, { cache: 'reload', mode: 'no-cors' })
-    .then(response => 
-      setPicture(link + new Date().getTime()))
+    .then( setPicture(link + new Date().getTime()) )
   }
   
 
   return(
     <div class="card">
+      <p>{(title).toUpperCase()}</p>
       <img src={picture}
       />
-      <div class="ButtLayout">
-        <a
-          onClick={() => reloadImg()}
-        >{button_name}</a>
-        <a
-          href={link} target="_blank"
-          class="link"
-        >
-          <img src={tabImg}/>
-        </a>
-      </div>
+      {
+        link.map((link, i) => (
+        <div class="ButtLayout">
+          <button
+            onClick={() => reloadImg(link)}
+          >{link[1]}</button>
+          <button
+            onClick={() => window.open(link, '_blank')}
+            class="link"
+          >
+            <img src={tabImg}/>
+          </button>
+        </div>
+        ))
+      }
     </div>
   )
 }
--- src/pages/play/effects.js
+++ src/pages/play/effects.js
@@ -13,7 +13,8 @@ function Effects(){
       title: "Quandrix Recall Codex",
       img_link: require('../../assets/display_cards/Quandrix Recall Codex.png'),
       card_link: [
-        "https://api.scryfall.com/cards/random?q=t:creature+m%3D{4}{G}{G}+-is:digital&unique=cards&format=image#"
+        ["https://api.scryfall.com/cards/random?q=t:creature+m%3D{4}{G}{G}+-is:digital&unique=cards&format=image#",
+         "GENERATE DREADMAW"]
       ],
     },
 
@@ -21,7 +22,8 @@ function Effects(){
       title: "Trifle Mage",
       img_link: require('../../assets/display_cards/Trifle Mage.png'),
       card_link: [
-        "https://api.scryfall.com/cards/random?q=is:spell+t:artifact+c%3Dc+cmc%3C%3D1+f%3Dv&unique=cards&format=image#"
+        ["https://api.scryfall.com/cards/random?q=is:spell+t:artifact+c%3Dc+cmc%3C%3D1+f%3Dv&unique=cards&format=image#",
+        "GENERATE BAUBLE"]
       ],
       link_name: ""
     },
@@ -30,7 +32,41 @@ function Effects(){
       title: "Sheet Head",
       img_link: require('../../assets/display_cards/Sheet Head.png'),
       card_link: [
-        "https://api.scryfall.com/cards/random?q=t:sticker&unique=cards&format=image#"
+        ["https://api.scryfall.com/cards/random?q=t:sticker&unique=cards&format=image#",
+        "GENERATE STICKERS"]
+      ],
+    },
+
+    {
+      title: "Blade of This or That",
+      img_link: require('../../assets/display_cards/Blade of This or That.png'),
+      card_link: [
+        ["https://api.scryfall.com/cards/random?q=otag:cycle-sword-ally-color+or+otag:cycle-sword-enemy-color&unique=cards&format=image#",
+        "GENERATE SWORD"]
+      ],
+    },
+
+    {
+      title: "Omenpath Awakener",
+      img_link: require('../../assets/display_cards/Omenpath Awakener.png'),
+      card_link: [
+        ["https://api.scryfall.com/cards/random?q=is:planar&unique=cards&format=image#",
+        "GENERATE PLANE"]
+      ],
+    },
+
+    {
+      title: "Curm Trashmaster Mage",
+      img_link: require('../../assets/display_cards/Curm Trashmaster Mage.png'),
+      card_link: [
+        ["https://api.scryfall.com/cards/random?q=is:spell+t:equipment+c:c+r:c+-is:digital&&unique=cards&format=image#",
+        "GENERATE COMMON"],
+        ["https://api.scryfall.com/cards/random?q=is:spell+t:equipment+c:c+r:u+-is:digital&&unique=cards&format=image#",
+        "GENERATE UNCOMMON"],
+        ["https://api.scryfall.com/cards/random?q=is:spell+t:equipment+c:c+r:r+-is:digital&&unique=cards&format=image#",
+        "GENERATE RARE"],
+        ["https://api.scryfall.com/cards/random?q=is:spell+t:equipment+c:c+r:m+-is:digital&&unique=cards&format=image#",
+        "GENERATE MYTHIC"],
       ],
     },
   ])
@@ -46,7 +82,6 @@ function Effects(){
                   title={card.title}
                   image={card.img_link}
                   link={card.card_link}
-                  button_name="GENERATE"
                 />
               ))
             }
