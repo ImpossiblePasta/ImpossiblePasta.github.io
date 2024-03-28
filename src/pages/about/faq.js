f3e1cbbf8893a5f6b7cbcdf7f7a9a884d91e92f5
ImpossiblePasta <joshuamedves@gmail.com>
2024-03-28 19:01:02 -0400

    FAQ Funny

--- src/pages/about/faq.js
+++ src/pages/about/faq.js
@@ -31,15 +31,94 @@ function FAQ(){
                     } />
                 <Collapse id='Why'
                     heading={
-                        <p>Why Is This Site Unfinished?</p> 
+                        <p>Why Make An About Page With No Real Answers?</p> 
                     }
                     children={
                         <div>
-                            <p> Sometimes you decide to make a site from scratch when you have little experience.
-                                And sometimes you decide to host the site on github pages and it causes way more 
-                                of a hassel than you expected.
+                            <p> Well, when I started making the site an FAQ seemed like a good idea.
                             </p>
-                            <p> The site will be worked on until April 1st.
+                        </div>
+                    } />
+                    <Collapse id='Brain No Work'
+                    heading={
+                        <p>Why Are There So Many Spelling Mistakes And Errors?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> Dyslexia is a hell of a drug.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='More'
+                    heading={
+                        <p>Are You Going To Continue Making Sets?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> Always. My next project is polishing an older set I made. Then releasing it with accesible formatting. Also UnAmused 2, UnFunny, is currently
+                                in the works.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='Patch'
+                    heading={
+                        <p>Will You Make Any Changes To UnAmused After It Is Released?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> At first I was planning on making UnAmused 1.1. At this point, I am more intereseting in making a sequel set, and while
+                                I might change my mind, I think I could keep trying to improve UnAmused until the end of time. 
+                                UnAmused will stay how it is, quirks and all.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='Keep'
+                    heading={
+                        <p>How Long Do You Intend To Keep Answering These Pointless Questions?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> Until an FAQ page seems justified.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='Updates'
+                    heading={
+                        <p>Will You Continue To Add To The FAQ Page?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> If I'm asked any questions online, I will include the answer here. It's easy enough to do.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='Git'
+                    heading={
+                        <p>Why GitHub Pages?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> I'm a fool. A cheap fool.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='Funny'
+                    heading={
+                        <p>Do You Think You're Funny?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> God I hope.
+                            </p>
+                        </div>
+                    } />
+                    <Collapse id='Over'
+                    heading={
+                        <p>Is This FAQ Joke Over?</p> 
+                    }
+                    children={
+                        <div>
+                            <p> .
                             </p>
                         </div>
                     } />
